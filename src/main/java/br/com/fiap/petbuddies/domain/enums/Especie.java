2b64efacb1b5dab56d562374616e73ef649ccabd
gabriel <spbiel18@gmail.com>
2026-05-19 17:59:21 -0300

    fix: removing reptil from especie enum

--- src/main/java/br/com/fiap/petbuddies/domain/enums/Especie.java
+++ src/main/java/br/com/fiap/petbuddies/domain/enums/Especie.java
@@ -1,5 +1,5 @@
 package br.com.fiap.petbuddies.domain.enums;
 
 public enum Especie {
-    CACHORRO, GATO, PASSARO, COELHO, HAMSTER, REPTIL, OUTRO
+    CACHORRO, GATO, PASSARO, COELHO, HAMSTER, OUTRO
 }
